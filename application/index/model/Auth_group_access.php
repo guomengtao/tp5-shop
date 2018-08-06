e3945a0692367163e66fc1a01d7beb6d1c872e73
Tom <rinuo@qq.com>
2018-08-06 19:54:52 +0800

    增加了两个模型文件 测试模型关联

---
+++ application/index/model/Auth_group_access.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace app\index\model;
+
+use think\Model;
+use traits\model\SoftDelete;
+
+class Auth_group_access extends model
+{
+ 
+ 
+}
---
+++ application/index/model/User_profile.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\index\model;
+
+use think\Model;
+use traits\model\SoftDelete;
+class User_profile extends model
+{
+    
+
+
+
+}
