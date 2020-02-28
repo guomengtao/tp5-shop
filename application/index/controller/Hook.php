e67f4f316cf74f103a1d025ec6b224e757949c4e
guomengtao <rinuo@qq.com>
2020-02-28 17:56:28 +0800

    发现拉取文件也可以修改不影响

--- application/index/controller/Hook.php
+++ application/index/controller/Hook.php
@@ -11,11 +11,11 @@ class Hook
 {
     public function run()
     {
-        echo "开始拉取 测试结果";
+        echo "开始拉取 ";
 
         echo exec("git pull");
 
-
+        echo "拉取成功";
         // echo exec("git clone https://gitee.com/rinuo/phpmanual.git");
 
     }
