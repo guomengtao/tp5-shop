a0bbe47677e125e765ddbfd0b60d2d3879a8e301
guomengtao <rinuo@qq.com>
2020-02-28 20:44:19 +0800

    更新留言提交方式

--- application/index/controller/Bbs.php
+++ application/index/controller/Bbs.php
@@ -179,8 +179,8 @@ class Bbs extends \think\Controller
                 ->find();
 
 //            echo '<h5 class="text-center"><small>您已累计在线'.$user['online_time'].'分钟</small></h5>';
-
-            $user->online_time = $user['online_time'] + 3;
+            $online_time = isset($user['online_time'])?$user['online_time']:0;
+            $user->online_time = $online_time + 3;
 
             $user->save();
 
