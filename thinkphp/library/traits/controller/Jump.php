8aae6bc225c5d32147f927ba31a784e4c88d2e17
guomengtao <rinuo@qq.com>
2020-03-02 23:23:06 +0800

    tosefl

--- thinkphp/library/traits/controller/Jump.php
+++ thinkphp/library/traits/controller/Jump.php
@@ -11,7 +11,6 @@
  *     }
  * }
  */
-
 namespace traits\controller;
 
 use think\Config;
@@ -27,11 +26,11 @@ trait Jump
     /**
      * 操作成功跳转的快捷方法
      * @access protected
-     * @param mixed $msg 提示信息
-     * @param string $url 跳转的 URL 地址
-     * @param mixed $data 返回的数据
-     * @param int $wait 跳转等待时间
-     * @param array $header 发送的 Header 信息
+     * @param mixed  $msg    提示信息
+     * @param string $url    跳转的 URL 地址
+     * @param mixed  $data   返回的数据
+     * @param int    $wait   跳转等待时间
+     * @param array  $header 发送的 Header 信息
      * @return void
      * @throws HttpResponseException
      */
@@ -43,7 +42,7 @@ trait Jump
             $url = Url::build($url);
         }
 
-        $type   = $this->getResponseType();
+        $type = $this->getResponseType();
         $result = [
             'code' => 1,
             'msg'  => $msg,
@@ -54,7 +53,7 @@ trait Jump
 
         if ('html' == strtolower($type)) {
             $template = Config::get('template');
-            $view     = Config::get('view_replace_str');
+            $view = Config::get('view_replace_str');
 
             $result = ViewTemplate::instance($template, $view)
                 ->fetch(Config::get('dispatch_success_tmpl'), $result);
@@ -68,11 +67,11 @@ trait Jump
     /**
      * 操作错误跳转的快捷方法
      * @access protected
-     * @param mixed $msg 提示信息
-     * @param string $url 跳转的 URL 地址
-     * @param mixed $data 返回的数据
-     * @param int $wait 跳转等待时间
-     * @param array $header 发送的 Header 信息
+     * @param mixed  $msg    提示信息
+     * @param string $url    跳转的 URL 地址
+     * @param mixed  $data   返回的数据
+     * @param int    $wait   跳转等待时间
+     * @param array  $header 发送的 Header 信息
      * @return void
      * @throws HttpResponseException
      */
@@ -84,7 +83,7 @@ trait Jump
             $url = Url::build($url);
         }
 
-        $type   = $this->getResponseType();
+        $type = $this->getResponseType();
         $result = [
             'code' => 0,
             'msg'  => $msg,
@@ -95,7 +94,7 @@ trait Jump
 
         if ('html' == strtolower($type)) {
             $template = Config::get('template');
-            $view     = Config::get('view_replace_str');
+            $view = Config::get('view_replace_str');
 
             $result = ViewTemplate::instance($template, $view)
                 ->fetch(Config::get('dispatch_error_tmpl'), $result);
@@ -109,17 +108,17 @@ trait Jump
     /**
      * 返回封装后的 API 数据到客户端
      * @access protected
-     * @param mixed $data 要返回的数据
-     * @param int $code 返回的 code
-     * @param mixed $msg 提示信息
-     * @param string $type 返回数据格式
-     * @param array $header 发送的 Header 信息
+     * @param mixed  $data   要返回的数据
+     * @param int    $code   返回的 code
+     * @param mixed  $msg    提示信息
+     * @param string $type   返回数据格式
+     * @param array  $header 发送的 Header 信息
      * @return void
      * @throws HttpResponseException
      */
     protected function result($data, $code = 0, $msg = '', $type = '', array $header = [])
     {
-        $result   = [
+        $result = [
             'code' => $code,
             'msg'  => $msg,
             'time' => Request::instance()->server('REQUEST_TIME'),
@@ -134,10 +133,10 @@ trait Jump
     /**
      * URL 重定向
      * @access protected
-     * @param string $url 跳转的 URL 表达式
+     * @param string    $url    跳转的 URL 表达式
      * @param array|int $params 其它 URL 参数
-     * @param int $code http code
-     * @param array $with 隐式传参
+     * @param int       $code   http code
+     * @param array     $with   隐式传参
      * @return void
      * @throws HttpResponseException
      */
