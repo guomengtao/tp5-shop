146c1550f8bf3b47dbb8e77c28ef61c7860da70f
guomengtao <rinuo@qq.com>
2020-03-04 22:09:48 +0800

    autopush

--- application/index/controller/Api.php
+++ application/index/controller/Api.php
@@ -697,8 +697,8 @@ class Api extends \think\Controller
 
         echo $cha;
 
-        // $fp = file_get_contents($cha);
-        $fp = file_get_contents("https://api.chanyoo.net/sendsms?username=guomengtao1&password=KEYxtybMMdN&mobile=18210787405&content=验证码：3039【高血压】");
+        $fp = file_get_contents($cha);
+        // $fp = file_get_contents("https://api.chanyoo.net/sendsms?username=guomengtao1&password=KEYxtybMMdN&mobile=18210787405&content=验证码：3039【高血压】");
 
         dump($fp);
 
