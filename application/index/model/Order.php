2a29f7df5e8e77811a5061df786403aab05542df
guomengtao <rinuo@qq.com>
2020-03-13 04:05:38 +0800

    autopush

--- application/index/model/Order.php
+++ application/index/model/Order.php
@@ -16,26 +16,21 @@ class Order extends model
     protected $insert = ['name' => "游客", 'age' => 17, 'ip' => 17];
     protected $update = ['name'];
 
-    public function setNameAttr($value)
-    {
-        return request()->ip();
-        return "30";
-    }
+
 
     public function setIpAttr($value)
     {
         return request()->ip();
-        return "30";
     }
 
-    public function setAgeAttr($value)
-    {
-        return "20";
+    public function  user(){
+        $this->hasOne('user','id','user_id');
     }
+
+
 
     public function setTitleAttr($value)
     {
-        // return request()->ip();
         return strtolower($value);
     }
 
@@ -46,22 +41,10 @@ class Order extends model
 
     public function setShopAttr($value)
     {
-//        dump($value);die();
         return $value;
     }
 
-    public function getBuyAttr($value)
-    {
-        // 查询播放记录条数
-        $play_count = Video::where('shop', '=', Shop)
-            ->where('id', '<>', 5)
-            ->count();
-        $play_count = 999;
-        dump($play_count);
-        die();
-        return 999;
 
-    }
 
     public static function registration_today()
     {
@@ -72,30 +55,11 @@ class Order extends model
         return $registration;
     }
 
-    public function getTitleAttr($value)
-    {
 
-
-        if ($value == 1) {
-
-            return "男";
-        } elseif ($value == 2) {
-
-            return "女";
-        } else {
-
-            return $value . "";
-
-        }
-
-
-    }
 
     public function getContentAttr($value)
     {
-        // $title = [-1=>'删除',0=>'禁用',1=>'正常',2=>'待审核'];
         if ($value) {
-            # code...
             return "<>" . $value;
 
         }
