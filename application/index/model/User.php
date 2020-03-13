c2c589cebd8cf69795c950409310b512a857a032
guomengtao <rinuo@qq.com>
2020-03-13 21:15:11 +0800

    autopush

--- application/index/model/User.php
+++ application/index/model/User.php
@@ -107,8 +107,7 @@ class User extends model
 
     public function userQq()
     {
-        return $this->hasOne('userQq');
-        //hasOne('关联模型名','外键名','主键名',['模型别名定义'],'join类型');
+        return $this->hasOne('UserQq', 'user_id', 'id');
     }
     public function footprint()
     {
