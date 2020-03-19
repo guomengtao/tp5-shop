d16a5bea9363e2e14e680498ab96c191ddb225b9
guomengtao <rinuo@qq.com>
2020-03-20 01:23:51 +0800

    autopush

--- application/index/controller/User.php
+++ application/index/controller/User.php
@@ -149,6 +149,9 @@ class User extends Frontend
             $table = QueryList::post($url, ['ip' => $ip])->find('table');
         } catch (\Exception $e) {
             if ($web) {
+                dump($e);
+                echo "goto2";
+
                 // 调用2号接口 http://tp5.dq.gaoxueya.com/index/user/humanapi/api/ip/223.96.76.158
                 $url = "http://tp5.dq.gaoxueya.com/index/user/humanapi/ip/".$ip;
                 $arr = file_get_contents($url);
