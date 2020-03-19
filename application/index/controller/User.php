8a68bb637e21698de689ca1637f0a1954355111e
guomengtao <rinuo@qq.com>
2020-03-20 01:27:51 +0800

    autopush

--- application/index/controller/User.php
+++ application/index/controller/User.php
@@ -74,13 +74,13 @@ class User extends Frontend
     public function human($ip = '119.62.42.104')
     {
         $web = input('web');
-        if ($web){
+        if ($web) {
             echo "linking";
         }
 
         $footprint = Footprint::where('ip', $ip)->count();
         if (!$footprint) {
-            if ($web){
+            if ($web) {
                 echo "null";
             }
             return "";
@@ -148,14 +148,25 @@ class User extends Frontend
         try {
             $table = QueryList::post($url, ['ip' => $ip])->find('table');
         } catch (\Exception $e) {
+            
+            // 调用2号接口 http://tp5.dq.gaoxueya.com/index/user/humanapi/api/ip/223.96.76.158
+            $url = "http://tp5.dq.gaoxueya.com/index/user/humanapi/ip/".$ip;
+            $arr = file_get_contents($url);
+            $arr = json_decode($arr, true);
+
+            if ($arr['address']) {
+                $this->save($arr, $ip);
+            }
+
             if ($web) {
                 dump($e);
                 echo "goto2";
-
+                dump($arr);
                 // 调用2号接口 http://tp5.dq.gaoxueya.com/index/user/humanapi/api/ip/223.96.76.158
                 $url = "http://tp5.dq.gaoxueya.com/index/user/humanapi/ip/".$ip;
                 $arr = file_get_contents($url);
                 $arr = json_decode($arr, true);
+                dump($arr);
                 if ($arr['address']) {
                     $this->save($arr, $ip);
                 }
