c617e295c754231cccb660b1e0e4fdd223405b73
guomengtao <rinuo@qq.com>
2020-03-20 21:41:37 +0800

    autopush

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1908,9 +1908,7 @@ class Index extends Frontend
          */
 
         $ip              = Request::instance()->ip();
-        echo $ip;
         $human           = Ipinfo::where('ip', $ip)->find();
-        dump($human);
         $views_today     = Footprint::views_today();
         $views_yesterday = Footprint::views_yesterday();
         // 查询今天签到
